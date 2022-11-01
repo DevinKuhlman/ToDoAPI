f33fb7a284255a6b9580e4c9fa9ecdc67e556e6c
DevinKuhlman <devin.kuhlman@att.net>
2022-11-01 12:50:48 -0500

    Add project files.

---
+++ ToDoAPI/ToDoAPI/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+using Microsoft.AspNetCore.Cors;//Cors Functionality
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("OriginPolicy", "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoDB"));//The string should match the connectionString name in appsettings.json
+    }
+    );
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors();
+
+app.Run();
